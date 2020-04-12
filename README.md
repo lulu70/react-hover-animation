ec684b06e832677760fe66428965d49ab6fb23ab
lulu70 <liorco70@yahoo.com>
2020-04-12 12:59:50 +0200

    initial-commit

---
+++ README.md
@@ -0,0 +1,163 @@
+# react-hover-animation
+
+> A react wrapper component that detects hover and adds animation from the awesome [react-spring](https://www.react-spring.io/).
+> Works both on the web and mobile touch events.
+
+[![NPM](https://img.shields.io/npm/v/react-hover-animation.svg)](https://www.npmjs.com/package/react-hover-animation) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+<!-- ## Example:
+
+[https://lulu70.github.io/react-hover-animation/](https://lulu70.github.io/react-hover-animation/) -->
+
+## Install
+
+```bash
+npm install --save react-hover-animation
+```
+
+## Usage
+
+### Basic usage
+
+```jsx
+import React from 'react'
+import { AnimationWrapper } from 'react-hover-animation'
+
+const App = () => {
+  return (
+    <div>
+      <AnimationWrapper>
+        <h1>I animate on hover</h1>
+      </AnimationWrapper>
+    </div>
+  )
+}
+export default App
+```
+
+### Optional props
+
+| Name               | Type   | Default |
+| ------------------ | ------ | ------- |
+| `style`            | object | none    |
+| `minimumOpacity`   | number | 0.6     |
+| `minimumScale`     | number | 0.95    |
+| `colors`           | object | none    |
+| `backgroundColors` | object | none    |
+
+- `style`: Style object for the wrapper component - _styles will overrides the hover styles_.
+- `minimumOpacity`: The minimum opacity of the wrapper component on hover animation - _set to 1 for no opacity change_.
+- `minimumScale`: The minimum scale of the wrapper component on hover animation - _set to 1 for no scale change_.
+- `colors`: The hover and normal font colors **must provide two values**.
+  - `hoverColor`: The hover font color.
+  - `color`: The normal font color.
+- `backgroundColors`: The hover and normal backgroundColors **must provide two values**.
+  - `hoverColor`: The hover background color.
+  - `color`: The normal background color.
+
+### Usage with props
+
+```jsx
+import React from 'react'
+import { AnimationWrapper } from 'react-hover-animation'
+
+const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        backgroundColor: 'grey',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <AnimationWrapper
+        className='animation-wrapper'
+        // styles will overrides the hover styles
+        style={{
+          textAlign: 'center',
+          borderRadius: '5px',
+          padding: '5px',
+        }}
+        // set to 1 for no opacity change
+        minimumOpacity={0.6}
+        // set to 1 for no scale change
+        minimumScale={0.95}
+        // must provide two values
+        colors={{
+          color: 'black',
+          hoverColor: 'white',
+        }}
+        // must provide two values
+        backgroundColors={{
+          color: 'white',
+          hoverColor: 'black',
+        }}
+      >
+        <h1>I animate on hover</h1>
+      </AnimationWrapper>
+    </div>
+  )
+}
+export default App
+```
+
+### The useHover hook
+
+If you don't want to render a wrapper div you can also import a custom hook and apply the animation directly on the element.
+same props can be passed as an object to the hook.
+
+```jsx
+import React from 'react'
+import { useHover } from 'react-hover-animation'
+
+const WithHook = () => {
+  const { spring, animated, setHover } = useHover({
+    // optional props
+    minimumOpacity: 0.6,
+    minimumScale: 0.95,
+    colors: {
+      color: 'black',
+      hoverColor: 'white',
+    },
+    backgroundColors: {
+      color: 'white',
+      hoverColor: 'black',
+    },
+  })
+  return (
+    <div
+      style={{
+        height: '100vh',
+        backgroundColor: 'grey',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {/* add  (animated) to the element */}
+      <animated.h1
+        // add spring to the style object
+        style={spring}
+        // add these two event handlers
+        onPointerOver={() => {
+          setHover(true)
+        }}
+        onPointerOut={() => {
+          setHover(false)
+        }}
+      >
+        I animate on hover
+      </animated.h1>
+    </div>
+  )
+}
+export default WithHook
+```
+
+## License
+
+MIT © [lulu70](https://github.com/lulu70)
+
+---
---
+++ index.js
@@ -0,0 +1,109 @@
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+var reactSpring = require('react-spring');
+var PropTypes = require('prop-types');
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+
+var __assign = function() {
+    __assign = Object.assign || function __assign(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+
+function __rest(s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+}
+
+var useHover = function (_a) {
+    var _b = _a === void 0 ? {} : _a, minimumScale = _b.minimumScale, minimumOpacity = _b.minimumOpacity, colors = _b.colors, backgroundColors = _b.backgroundColors;
+    var _c = React.useState(false), hover = _c[0], setHover = _c[1];
+    var hasTwoColors = colors && colors.color && colors.hoverColor && true;
+    var hasTwoBGColors = backgroundColors &&
+        backgroundColors.color &&
+        backgroundColors.hoverColor &&
+        true;
+    var minScale = minimumScale ? minimumScale : 0.95;
+    var minOpacity = minimumOpacity ? minimumOpacity : 0.6;
+    var springInitialProps = {
+        transform: "scale(1)",
+        opacity: '1',
+        color: colors && hasTwoColors ? colors.color : '',
+        backgroundColor: backgroundColors && hasTwoBGColors ? backgroundColors.color : '',
+    };
+    var _d = reactSpring.useSpring(function () { return springInitialProps; }), spring = _d[0], setSpring = _d[1];
+    setSpring({
+        transform: "scale(" + (hover ? minScale : '1') + ")",
+        opacity: hover ? "" + minOpacity : '1',
+        color: colors && hasTwoColors ? (hover ? colors.hoverColor : colors.color) : '',
+        backgroundColor: backgroundColors && hasTwoBGColors
+            ? hover
+                ? backgroundColors.hoverColor
+                : backgroundColors.color
+            : '',
+    });
+    return {
+        setHover: setHover,
+        spring: spring,
+        animated: reactSpring.animated,
+    };
+};
+
+var AnimationWrapper = function (_a) {
+    var colors = _a.colors, backgroundColors = _a.backgroundColors, minimumScale = _a.minimumScale, minimumOpacity = _a.minimumOpacity, style = _a.style, props = __rest(_a, ["colors", "backgroundColors", "minimumScale", "minimumOpacity", "style"]);
+    var _b = useHover({
+        minimumScale: minimumScale,
+        colors: colors,
+        backgroundColors: backgroundColors,
+        minimumOpacity: minimumOpacity,
+    }), animated = _b.animated, setHover = _b.setHover, spring = _b.spring;
+    return (React.createElement(animated.div, __assign({}, props, { style: __assign(__assign({ display: 'inline-block' }, spring), style), onPointerOver: function () {
+            setHover(true);
+        }, onPointerOut: function () {
+            setHover(false);
+        } })));
+};
+AnimationWrapper.propTypes = {
+    colors: PropTypes.shape({
+        color: PropTypes.string.isRequired,
+        hoverColor: PropTypes.string.isRequired,
+    }),
+    backgroundColors: PropTypes.shape({
+        color: PropTypes.string.isRequired,
+        hoverColor: PropTypes.string.isRequired,
+    }),
+    minimumScale: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    minimumOpacity: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    style: PropTypes.object,
+};
+
+exports.AnimationWrapper = AnimationWrapper;
+exports.useHover = useHover;
+//# sourceMappingURL=index.js.map
